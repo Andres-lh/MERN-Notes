384b2594c75e4943c76a2e1c6dc67d8e835b4921
Andrés Lara <andres-lh@outlook.com>
2021-03-07 22:54:37 -0500

    Login done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,12 @@
 import Users from '../models/userModel.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const userRegistration = async (req, res) => {
+    const { username, email, password } = req.body;
     try {
-        const { username, email, password } = req.body;
         const user = await Users.findOne({email: email})
-        if(user) return res.status(400).json({msg: "The email already exists."})
+        if(user) return res.status(400).json({msg: "The email already exists."});
 
         const passwordHash = await bcrypt.hash(password, 10);
 
@@ -15,19 +16,46 @@ export const userRegistration = async (req, res) => {
             password: passwordHash
         })
         await newUser.save();
-        res.json(newUser);
+        res.json({msg: "Sign up succesful"});
     
+    } catch(err) {
+        return res.status(500).json({msg: err.message});
+    }
+    
+}
+
+export const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await Users.findOne({ email: email } );
+        if(!user) return res.status(400).json({msg: "User doesn't exist."});
+
+        const matchPassword = await bcrypt.compare(password, user.password);
+        if(!matchPassword) return res.status(400).json({msg: "Password is incorrect."});
+
+        const payload = { id: user._id, name: user.username};
+        const token = jwt.sign(payload, process.env.TOKEN, {expiresIn: "5h"});
+        res.json({token});
     } catch(err) {
         return res.status(500).json({msg: err.message})
     }
     
 }
 
-export const userLogin = (req, res) => {
+export const verifiedUser = (req, res) => {
     try {
-        res.json({ msg: 'Login'});
-    } catch(err) {
+        const token = req.header("Authorization");
+        if(!token) return res.send(false);
+
+        jwt.verify(token, process.env.TOKEN, async (err, verified) => {
+            if(err) return res.send(false);
+
+            const user = await Users.findById(verified.id)
+            if(!user) return res.send(false);
+
+            return res.send(true);
+        })
+    } catch (err) {
         return res.status(500).json({msg: err.message})
     }
-    
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { userRegistration, userLogin } from '../controllers/userController.js'
+import { userRegistration, userLogin, verifiedUser } from '../controllers/userController.js'
+
 const router = express.Router();
 
 router.post('/register', userRegistration)
-
 router.post('/login', userLogin)
+
+router.get('/verify', verifiedUser)
 
 export default router;
