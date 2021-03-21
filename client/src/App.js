3c44c261e71397af7a534a031d7a9bf594e9bcc8
Andrés Lara <andres-lh@outlook.com>
2021-03-21 18:20:13 -0500

    Working on login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css'
 import Login from './Components/Login';
@@ -5,10 +6,29 @@ import Notes from './Components/Notes';
 
 function App() {
     const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(()=>{
+        const checkLogin = async () =>{
+            const token = localStorage.getItem('tokenStore');
+            if(token){
+                const verified = await axios.get('/api/users/verify', {
+                    headers: {Authorization: token}
+                })
+                console.log(verified.data)
+                setIsLogin(verified.data);
+                if(verified.data === false) return localStorage.clear();
+            }else{
+                setIsLogin(false);
+            }
+        }
+        checkLogin();
+    }, [])
     return(
         <div>
             {
-                isLogin ? <Notes/> : <Login/>
+                isLogin 
+                ? <Notes /> 
+                : <Login setIsLogin={setIsLogin} />
             }
         </div>
     )
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import Notes from './Notes';
+import React, {     useState }from 'react';
+import axios from 'axios';
 import './styles/Login.css';
 
-function Login() {
+function Login({setIsLogin}) {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const [err, setErr] = useState('');
+
+    const onChangeInput = e =>{
+        const {name, value} = e.target;
+        setUser({...user, [name]: value});
+
+        setErr('');
+    }
+
+    const loginSubmit = async e => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('/api/users/login', {
+                email: user.email,
+                password: user.password
+            })
+            setUser({name: '', email: '', password: ''})
+            localStorage.setItem('tokenStore', res.data.token);
+            setIsLogin(true)
+        } catch (err) {
+            err.response.data.msg && setErr(err.response.data.msg)
+        }
+    }
+
+
     return(
         <div className = "login-container" >
             <div className= "login">
                 <h2>Login</h2>
-                <form>
+                <form onSubmit={loginSubmit}> 
                     <div className = "login-input">
-                        <input type="text" name="email" id="login-email" required/>
+                        <input type="text" name="email" id="email" value={user.email} required onChange={onChangeInput}/>
                         <label>Email</label>
                     </div>
                     <div className="login-input"> 
-                        <input type="password" name="password" id="user-password" required/>
+                        <input type="password" name="password" id="password" value={user.password} required onChange={onChangeInput}/>
                         <label>Password</label>
                     </div>
+                    <p>You don't have an account? <span> Register now</span></p> 
                     <button type="submit">Sign in</button>
+                    <h3>{err}</h3>
                 </form>
             </div>
             
-            <div>
-                 
-            </div>
+
         </div>
     )
 }
