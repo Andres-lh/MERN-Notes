cdfaa53c6c6252e32bee0700a2e4c3462848a0ac
Andrés Lara <andres-lh@outlook.com>
2021-03-22 18:32:46 -0500

    adding registration form

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -33,6 +33,20 @@ function Login({setIsLogin}) {
         }
     }
 
+    const registerSubmit = async e => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('/api/users/register', {
+                username: user.name,
+                email: user.email,
+                password: user.password
+            })
+            setUser({name: '', email: '', password: ''})
+            setErr(res.data.msg);
+        } catch (err) {
+            err.response.data.msg && setErr(err.response.data.msg)
+        }
+    }
 
     return(
         <div className = "login-container" >
@@ -48,12 +62,30 @@ function Login({setIsLogin}) {
                         <label>Password</label>
                     </div>
                     <p>You don't have an account? <span> Register now</span></p> 
-                    <button type="submit">Sign in</button>
+                    <button className="form-button" type="submit">Sign in</button>
                     <h3>{err}</h3>
                 </form>
             </div>
             
-
+            <div className="register">
+                <h2>Register</h2>
+                    <form onSubmit={registerSubmit}>
+                    <div className = "login-input">
+                            <input type="text" name="name" id="login-name" value={user.name} required onChange={onChangeInput}/>
+                            <label>Email</label>
+                        </div>
+                        <div className = "login-input">
+                            <input type="text" name="email" id="login-email" value={user.email} required onChange={onChangeInput}/>
+                            <label>Email</label>
+                        </div>
+                        <div className="login-input"> 
+                            <input type="password" name="password" id="user-password" value={user.password} required onChange={onChangeInput}/>
+                            <label>Password</label>
+                        </div>
+                        <button className="form-button" type="submit">Sign in</button>
+                        <h3>{err}</h3>
+                    </form>
+            </div>
         </div>
     )
 }
